b61f2b09e1f6fe50c43f53113d5c781fa9af8b07
Cory G Watson <gphat@cpan.org>
2011-11-27 22:21:57 +0000

    Import of GPHAT/Data-SearchEngine-ElasticSearch-0.10 from CPAN.
    gitpan-cpan-distribution: Data-SearchEngine-ElasticSearch
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         GPHAT/Data-SearchEngine-ElasticSearch-0.10.tar.gz
    gitpan-cpan-author:       GPHAT
    gitpan-cpan-maturity:     released

--- lib/Data/SearchEngine/ElasticSearch/Results.pm
+++ lib/Data/SearchEngine/ElasticSearch/Results.pm
@@ -1,6 +1,6 @@
 package Data::SearchEngine::ElasticSearch::Results;
 {
-  $Data::SearchEngine::ElasticSearch::Results::VERSION = '0.09';
+  $Data::SearchEngine::ElasticSearch::Results::VERSION = '0.10';
 }
 use Moose;
 
@@ -24,7 +24,7 @@ Data::SearchEngine::ElasticSearch::Results - Search Results
 
 =head1 VERSION
 
-version 0.09
+version 0.10
 
 =head1 AUTHOR
 
